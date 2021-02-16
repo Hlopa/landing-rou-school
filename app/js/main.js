95e75b0c116d085a7936316107a5f06d15aae675
Olga Barishevskaya <bariolga@yandex.ru>
2021-02-16 19:09:35 +0300

    create testimonials

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,24 @@
 'use scrict';
+
+$(function () {
+    $('.testimonial__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+    });
+});
+
+//Скролл на отзывах
+
+const sliderText = document.querySelectorAll('.slider__text');
+
+sliderText.forEach(item => {
+    const itemHeight = getComputedStyle(item).height;
+    const px = parseFloat(itemHeight);
+
+    if (px > 250) {
+        item.style.cssText = `overflow-y: scroll; max-height: 250px`;
+
+    }
+})
